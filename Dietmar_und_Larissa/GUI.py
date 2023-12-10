b6705adbedba9301e3e6a821b07cf87b42ca7c58
Dietmar Ysop <118566330+DYsop@users.noreply.github.com>
2023-12-10 13:26:59 +0100

    Update GUI.py

--- Dietmar_und_Larissa/GUI.py
+++ Dietmar_und_Larissa/GUI.py
@@ -10,13 +10,12 @@ from tkinter import messagebox
 # Integration Funktionen Phil Balczukat
 #import Phil.basic_features_lib
 
-# Globale Variable für properties_label
-properties_label = None
+# Variablen für Bildeigenschaften global definieren
+label_Bildbreite = None
+label_Bildhöhe = None
+label_Dateipfad = None
 
 def show_help_dialog():
-    help_dialog = tk.Toplevel(root)
-    help_dialog.title("Hilfe")
-
     help_text = """
     Hier sind die Funktionen des Programms:
 
@@ -43,16 +42,12 @@ def show_help_dialog():
        - Funktion 2: Kurze Beschreibung der Funktion.
        - ...
 
-    5. OCR Erkennung:
+    5. OCR Funktionen:
        - Funktion 1: Kurze Beschreibung der Funktion.
        - Funktion 2: Kurze Beschreibung der Funktion.
        - ...
     """
-    help_label = tk.Label(help_dialog, text=help_text, justify='left')
-    help_label.pack(padx=20, pady=20)
-
-    close_button = tk.Button(help_dialog, text="Schließen", command=help_dialog.destroy)
-    close_button.pack(pady=10)
+    messagebox.showinfo("Hilfe", help_text)
 
 def print_me():
     print(f"Datei: {file_path.get()}")
@@ -67,47 +62,24 @@ def center_window(window):
     y_position = (screen_height - window_height) // 2
     window.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
 
-def close_program():
-    print("Programm wird geschlossen.")
-    root.destroy()
-
 def show_info_dialog():
-    info_dialog = tk.Toplevel(root)
-    info_dialog.title("BILDBEARBEITUNG UND BILDANALYSE")
-    info_dialog.geometry("600x600")
-
-    tk.Label(info_dialog, text="BILDBEARBEITUNG UND BILDANALYSE").pack()
-    tk.Frame(info_dialog, height=2, bd=1, relief="sunken").pack(fill="x", padx=10, pady=5)
-    tk.Label(info_dialog, text="Entwicklerteam Gruppe a1-1:").pack(pady=5)
-
-    developers = ["Al-Atrash, Anas Abdelsaman Ramadan", "Balczukat, Phil", "Ferme, Larissa", "Koch, Claus-Peter",
-                  "Ysop, Dietmar"]
-    for developer in developers:
-        tk.Label(info_dialog, text=developer).pack(pady=1)
-
-    tk.Frame(info_dialog, height=2, bd=1, relief="sunken").pack(fill="x", padx=10, pady=5)
-
-    center_window(info_dialog)
+    message = "Entwicklerteam Gruppe a1-1:\n\n" + "\n".join([
+        "Al-Atrash, Anas Abdelsaman Ramadan",
+        "Balczukat, Phil",
+        "Ferme, Larissa",
+        "Koch, Claus-Peter",
+        "Ysop, Dietmar"
+    ])
+    messagebox.showinfo("BILDBEARBEITUNG UND BILDANALYSE", message)
 
 def show_version_dialog():
-    version_dialog = tk.Toplevel(root)
-    version_dialog.title("BILDBEARBEITUNG UND BILDANALYSE")
-    version_dialog.geometry("280x160")
-    tk.Label(version_dialog, text="BILDBEARBEITUNG UND BILDANALYSE").pack()
-    tk.Frame(version_dialog, height=2, bd=1, relief="sunken").pack(fill="x", padx=10, pady=5)
-    tk.Label(version_dialog, text="Programmversion 3.0").pack(pady=1)
-    tk.Label(version_dialog, text="(c) 2024").pack(pady=3)
+    version_text = """
+    BILDBEARBEITUNG UND BILDANALYSE
 
-def save_file_callback():
-    file_path = filedialog.asksaveasfilename()
-    if file_path:
-        print(f"Datei speichern: {file_path}")
-
-def save_file_as_callback():
-    file_path = filedialog.asksaveasfilename()
-    if file_path:
-        print(f"Datei speichern unter: {file_path}")
-        # Hier könntest du die Logik für das Speichern des aktuellen Bildes an einem anderen Ort implementieren
+    Programmversion 3.0
+    (c) 2024
+    """
+    messagebox.showinfo("Version", version_text)
 
 #Funktion, um den Dateidialog zu öffnen und ein Bild zu laden
 def show_file_dialog():
@@ -115,9 +87,10 @@ def show_file_dialog():
     filename = filedialog.askopenfilename(filetypes=file_types)
     show_image(filename)
 
+#Funktion um das ausgewählte Bild zu laden und die Dateieigenschaften zuzuweisen
 def show_image(image_path):
     original_image = cv2.imread(image_path)
-    resized_image = cv2.resize(original_image,(495,600))
+    resized_image = cv2.resize(original_image, (495, 600))
 
     # Konvertiere das Bild von BGR zu RGB (für die Anzeige in Tkinter)
     rgb_image = cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB)
@@ -130,37 +103,42 @@ def show_image(image_path):
     canvas.place(x=35, y=65)
     canvas.create_image(0, 0, anchor=tk.NW, image=tk_image)
 
-    # Zeige Bildeigenschaften in einem Textfeld an
-    properties_text = f"Dateipfad: {image_path}\n" \
-                      f"Bildbreite: {original_image.shape[1]}\n" \
-                      f"Bildhöhe: {original_image.shape[0]}"
-    properties_label.config(text=properties_text)
+    # Zeige Bildeigenschaften in den globalen Label-Variablen an
+    label_Bildbreite = original_image.shape[1]
+    label_Bildhöhe = original_image.shape[0]
+    label_Dateipfad = image_path
 
-    # Textfeld für Bildeigenschaften erstellen
-    properties_label = tk.Label(root, text="")
-    properties_label.place(x=30, y=60)
+    # Zeige Bildeigenschaften in den globalen Label-Variablen an
+    anzeigen_Bildbreite.config(text=f"Bildbreite: {label_Bildbreite:,} Pixel")
+    anzeigen_Bildhöhe.config(text=f"Bildhöhe: {label_Bildhöhe:,} Pixel")
+    anzeigen_Dateipfad.config(text=f"Dateipfad: {label_Dateipfad.replace(',', '.')}")
 
     # Halte das Tkinter-Fenster offen
     root.mainloop()
 
-def reset_canvas():
-    # Erstelle ein leeres Bild (weißer Hintergrund)
-    empty_image = Image.new("RGB", (500, 500), "white")
-    tk_empty_image = ImageTk.PhotoImage(empty_image)
+#Funktion, um speichern aufzurufen
+def save_file_callback():
+    file_path = filedialog.asksaveasfilename()
+    if file_path:
+        print(f"Datei speichern: {file_path}")
 
-    # Aktualisiere das Canvas mit dem leeren Bild
-    canvas.itemconfig(image_item, image=tk_empty_image)
+#Funktion, um speichern unter aufzurufen
+def save_file_as_callback():
+    file_path = filedialog.asksaveasfilename()
+    if file_path:
+        print(f"Datei speichern unter: {file_path}")
+
+#Funktion, um das Programm zu schließen
+def close_program():
+    print("Programm wird geschlossen.")
+    root.destroy()
+
+def reset_canvas():
+    # Lösche alle Elemente im Canvas
+    canvas.delete("all")
 
 def print_to_pdf():
-    try:
-        global image_tk
-        if image_tk:
-            pdf = FPDF()
-            pdf.add_page()
-            pdf.image(ImageTk.getimage(image_tk), x=10, y=10, w=190)
-            pdf.output("output.pdf")
-    except NameError:
-        print("Bild nicht gefunden.")
+    pass
 
 # Tkinter-App erstellen
 root = tk.Tk()
@@ -169,14 +147,14 @@ root.title("BILDBEARBEITUNG UND BILDANALYSE")
 # Hauptfenster in der Bildschirmmitte positionieren
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
-window_width = 1300
+window_width = 1280
 window_height = 800
 x_position = (screen_width - window_width) // 2
 y_position = (screen_height - window_height) // 2
 root.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
 
 # Erstelle ein Canvas und platziere es auf dem Fenster
-canvas = tk.Canvas(root, width=600, height=685, bd=2, relief="solid")
+canvas = tk.Canvas(root, width=600, height=650, bd=2, relief="solid")
 canvas.place(x=30, y=60)
 
 # Menüleiste erstellen
@@ -187,16 +165,12 @@ file_menu.add_command(label="Datei öffnen", command=show_file_dialog)
 file_menu.add_command(label="Datei speichern", command=save_file_callback)
 file_menu.add_command(label="Datei speichern unter", command=save_file_as_callback)
 file_menu.add_separator()
-file_menu.add_command(label="Datei schließen", command=print_me)
-file_menu.add_separator()
 file_menu.add_command(label="PDF Drucken", command=print_to_pdf())
-file_menu.add_separator()
-file_menu.add_command(label="Eigenschaften", command=print_me)
 file_menu.add_separator()
 file_menu.add_command(label="Ende", command=close_program)
 
 edit_menu = tk.Menu(menu_bar, tearoff=0)
-edit_menu.add_command(label="Reset / Default", command=print_me)
+edit_menu.add_command(label="Default Werte", command=print_me)
 
 settings_menu = tk.Menu(menu_bar, tearoff=0)
 settings_menu.add_command(label="Standardeinstellungen", command=print_me)
@@ -219,6 +193,16 @@ root.config(menu=menu_bar)
 # Button zum Zurücksetzen des Canvas hinzufügen
 reset_button = tk.Button(root, text="Reset Canvas", command=reset_canvas)
 reset_button.place(x=30,y=30)
+
+# Dateieigenschaften anzeigen
+anzeigen_Bildbreite = tk.Label(root, text=label_Bildbreite, bg="#e4eee9")
+anzeigen_Bildbreite.place(x=150, y=33)
+
+anzeigen_Bildhöhe = tk.Label(root, text=label_Bildbreite, bg="#e4eee9")
+anzeigen_Bildhöhe.place(x=300, y=33)
+
+anzeigen_Dateipfad = tk.Label(root, text=label_Dateipfad, bg="#e4eee9")
+anzeigen_Dateipfad.place(x=30, y=725)
 
 # Bereich für Bedienungselemente
 bedienung_frame = tk.Frame(root, width=300, height=800, bd=2, relief="solid", bg="#e4eee9")
@@ -247,12 +231,12 @@ tk.Label(erweitert_frame, text="Erweiterte Funktionen", bg="#eeeee4").pack()
 # Bereitstellung der Buttons (Erweitert) und nebeneinander anordnen
 buttons_frame = tk.Frame(erweitert_frame)
 buttons_frame.pack()
-tk.Button(buttons_frame, text="Funktion 1", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
-tk.Button(buttons_frame, text="Funktion 2", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
-tk.Button(buttons_frame, text="Funktion 3", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
-tk.Button(buttons_frame, text="Funktion 4", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
-tk.Button(buttons_frame, text="Funktion 5", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
-tk.Button(buttons_frame, text="Funktion 6", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
+tk.Button(buttons_frame, text="Markup", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
+tk.Button(buttons_frame, text="Weich-\n zeichner", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
+tk.Button(buttons_frame, text="Licht\n Schatten", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
+tk.Button(buttons_frame, text="Texteffekte", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
+tk.Button(buttons_frame, text="Farbeffekte", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
+tk.Button(buttons_frame, text="Filter", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
 
 # Bereich für Bilderkennung und -transformation
 bilderkennung_frame = tk.Frame(bedienung_frame, width=120, height=30, bd=1, relief="sunken", bg="#eeeee4")
@@ -287,17 +271,17 @@ tk.Button(buttons_frame, text="Funktion 6", command=print_me, width=10, height=3
 # Bereich für OCR Erkennung
 OCR_frame = tk.Frame(bedienung_frame, width=120, height=30, bd=1, relief="sunken", bg="#eeeee4")
 OCR_frame.pack(pady=40)
-tk.Label(OCR_frame, text="OCR Erkennung", bg="#eeeee4").pack()
+tk.Label(OCR_frame, text="OCR Funktionen", bg="#eeeee4").pack()
 
 # Bereitstellung der Buttons (Erweitert) und nebeneinander anordnen
 buttons_frame = tk.Frame(OCR_frame)
 buttons_frame.pack()
-tk.Button(buttons_frame, text="Funktion 1", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
-tk.Button(buttons_frame, text="Funktion 2", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
-tk.Button(buttons_frame, text="Funktion 3", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
-tk.Button(buttons_frame, text="Funktion 4", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
-tk.Button(buttons_frame, text="Funktion 5", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
-tk.Button(buttons_frame, text="Funktion 6", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
+tk.Button(buttons_frame, text="Text-\n erkennung", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
+tk.Button(buttons_frame, text="Text\n to speech", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
+tk.Button(buttons_frame, text="Text\n to pdf", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
+tk.Button(buttons_frame, text="Sprach-\n erkennung", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
+tk.Button(buttons_frame, text="Dummy", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
+tk.Button(buttons_frame, text="Dummy", command=print_me, width=10, height=3, bg="#e4e4ee").pack(side="left", padx=5)
 
 root.config(bg="#fdfdfc")
 root.mainloop()
